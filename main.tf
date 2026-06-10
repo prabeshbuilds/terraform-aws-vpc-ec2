8b78017b16b2b8161d54ca699c5daf202570229b
prabeshbuilds <prabeshuser407@gmail.com>
2026-06-10 15:17:53 +0545

    Updated the s3 bucket

--- main.tf
+++ main.tf
@@ -1,125 +1,23 @@
-#####
-# Terraform configuration for AWS resources
-#####
+# 1. Get current AWS account ID
+data "aws_caller_identity" "current" {}
 
-resource "aws_VPC" "main"{
-  cidr_block = var.vpc_cidr
+# 2. Define the S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  # The bucket name must be globally unique across all AWS accounts
+  bucket = "my-terraform-bucket-${data.aws_caller_identity.current.account_id}-2026" 
+
   tags = {
-    Name = "main_vpc"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
   }
 }
 
+# 3. Block Public Access (Best Practice)
+resource "aws_s3_bucket_public_access_block" "my_bucket_privacy" {
+  bucket = aws_s3_bucket.my_bucket.id
 
-# Internet gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "main_igw"
-  }
-
-
-# Public subnet
-resource "aws_subnet" "public" {
-  vpc_id           = aws_vpc.main.id
-  cidr_block       = var.public_subnet_cidr
-  availability_zone = var.az
-  tags = {
-    Name = "public_subnet"
-  }
-} 
-
-
-# Private subnet
-resource "aws subnet" "private" {
-  vpc_id           = aws_vpc.main.id
-  cidr_block       = var.private_subnet_cidr
-  availability_zone = var.az
-  tags = {
-    Name = "private_subnet"
-  }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
-
-# Public route table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "public_rt"
-  }
-}
-
-
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-
-# private route table
-resource "aws route table" "private"{
-  vpc_id = aws_vpc.main.id
-  rou te {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "private_rt"
-  }
-
-}
-
-
-
-resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
-
-
-# Security group for EC2 instances
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2_security_group"
-  description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0./0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "ec2_sg"
-  }
-
-  resource "aws_instance" "web" {
-    ami           = var.ami_id
-    instance_type = var.instance_type
-    subnet_id     = aws_subnet.public.id
-    security_groups = [aws_security_group.ec2_sg.name]
-    tags = {
-      Name = "web_instance"
-    }
-  }
-
-    
--- variables.tf
+++ variables.tf
@@ -1,28 +1,6 @@
-variable "aws_region"{
-  default = "us-east-1"
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr" {
-  default = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidr" {
-  default = "10.0.2.0/24"
-}
-
-variable "az"{
-  default = "us-east-1a"
-}
-
-variable "ami_id" {
-  default = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI (HVM), SSD Volume Type
-}
-
-variable "instance_type" {
-  default = "t2.micro"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
