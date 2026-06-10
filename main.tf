8d935d2dc13063674d8aa1cf8eabd681a724919e
prabeshbuilds <prabeshuser407@gmail.com>
2026-06-10 16:24:52 +0545

    Configure the server

--- main.tf
+++ main.tf
@@ -21,3 +21,64 @@ resource "aws_s3_bucket_public_access_block" "my_bucket_privacy" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+
+# 4. Default VPC lookup
+data "aws_vpc" "default" {
+  default = true
+}
+
+# 5. Security Group for EC2 Instance
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-instance-sg"
+  description = "Allow SSH and HTTP to EC2 instance"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-instance-sg"
+  }
+}
+
+# 6. EC2 Instance
+resource "aws_instance" "my_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "terraform-ec2-instance"
+  }
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.my_instance.id
+}
+
+output "ec2_instance_public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,26 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+
+  validation {
+    condition     = contains(["t2.micro", "t3.micro", "t4g.micro"], var.instance_type)
+    error_message = "instance_type must be a free-tier eligible micro instance (t2.micro, t3.micro, or t4g.micro)."
+  }
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0152204c1a187337c"
+}
+
+variable "key_name" {
+  description = "Existing AWS EC2 key pair name for SSH access"
+  type        = string
+  default     = "my-key"
+}
+
